10186e5b720e40c81342d695aa74e2a26e63dc88
Roman Smirnov <smirnow3217831@gmail.com>
2023-08-21 03:41:37 +0300

    git pages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: "https://smirnovroman94.github.io/gitogramm/",
   transpileDependencies: true,
   pluginOptions: {
     storybook: {
